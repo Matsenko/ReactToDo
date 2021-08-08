3fe351d3bb44a024746e72f747a309fe6b5b07f4
Matsenko <vovamatsenko89@gmail.com>
2021-08-08 18:12:31 +0300

    Last update

--- src/ToDo/AddToDo.js
+++ src/ToDo/AddToDo.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types';
-export default function AddToDo(onCreate) {
+export default function AddToDo({onCreate}) {
     const [value, setValue] = useState('')
     function submitHandler(event) {
         event.preventDefault()
